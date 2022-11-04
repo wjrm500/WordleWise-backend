653193f9e6c59a38fa8299853ad04c23719d03ed
William May <wjrm500@gmail.com>
2022-11-04 09:49:44 +0800

    Supporting dev DB

--- database/aws/download_database.py
+++ database/aws/download_database.py
@@ -1,8 +1,12 @@
 import boto3
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 s3 = boto3.client('s3')
 BUCKET_NAME = 'wjrm500-wordle'
-OBJECT_NAME = 'wordle.db'
+OBJECT_NAME = os.environ.get('AWS_S3_OBJECT_NAME')
 FILE_NAME = OBJECT_NAME
 def download_database() -> None:
     print('Downloading SQLite database from AWS...')
