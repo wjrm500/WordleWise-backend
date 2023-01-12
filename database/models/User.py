b41941a1f38c50a7ae6581cf0de2b1fe0e12c8cf
William May <wjrm500@gmail.com>
2023-01-12 10:48:58 +0800

    Removing admin column from SQLAlchemy

--- database/models/User.py
+++ database/models/User.py
@@ -5,5 +5,4 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key = True, autoincrement = True)
     username = Column(String)
-    admin = Column(Integer)
     password_hash = Column(String)
