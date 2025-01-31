5dd670164acb1c4f0faa3d36cd9c30829568b3ca
William May <wjrm500@gmail.com>
2025-01-31 19:12:01 +0000

    Using local SQLite database not S3

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from http import HTTPStatus
 import os
 
 from database.Database import Database
-from database.aws.download_database import download_database
-from database.aws.upload_database import upload_database
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
@@ -28,12 +26,12 @@ def login():
     password = data['password']
     try:
         user = database.login(username, password)
-        access_token = create_access_token(identity = username, expires_delta = datetime.timedelta(minutes = 30))
+        access_token = create_access_token(identity=username, expires_delta=datetime.timedelta(minutes=30))
         return jsonify({'success': True, 'error': None, 'access_token': access_token, 'user': serialise_model(user)})
     except Exception as e:
         return jsonify({'success': False, 'error': str(e), 'access_token': None, 'user': None})
 
-@app.route('/getScores', methods = ['POST'])
+@app.route('/getScores', methods=['POST'])
 @jwt_required()
 def get_scores():
     try:
@@ -44,14 +42,13 @@ def get_scores():
         print(e)
         return jsonify(str(e), HTTPStatus.INTERNAL_SERVER_ERROR)
 
-@app.route('/addScore', methods = ['POST'])
+@app.route('/addScore', methods=['POST'])
 @jwt_required()
 def add_score():
     try:
         data = request.json
         database.set_timezone(data['timezone'])
         database.add_score(data['date'], data['user_id'], data['score'])
-        upload_database()
         resp = jsonify('')
         resp.headers.add('Access-Control-Allow-Origin', '*')
         return resp
@@ -69,22 +66,19 @@ def get_users():
         print(e)
         return jsonify(str(e), HTTPStatus.INTERNAL_SERVER_ERROR)
 
-@app.route('/executeSql', methods = ['POST'])
+@app.route('/executeSql', methods=['POST'])
 @jwt_required()
 def execute_sql():
     try:
         database.execute(request.json['sql'])
-        upload_database()
         resp = jsonify('')
         resp.headers.add('Access-Control-Allow-Origin', '*')
         return resp
     except Exception as e:
         return jsonify(str(e), HTTPStatus.INTERNAL_SERVER_ERROR)
 
-download_database()
-global database
-database_filename = os.environ.get('AWS_S3_OBJECT_NAME')
-database = Database(database_url = f'sqlite:///{database_filename}')
+database = Database(database_url='sqlite:///wordle-prod.db')
+
 if __name__ == '__main__':
     print("Starting server...")
     app.run(host="0.0.0.0", port=5000)
